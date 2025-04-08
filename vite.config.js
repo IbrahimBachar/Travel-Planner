6bc50cd1fb6e63a87c5c23acf8935e9c6e373e1d
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-04-08 23:20:41 +0200

    Project initiation

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  content:[
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+  ],
+
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'translateY(10px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+    },
+  },
+  plugins: [react(),
+    tailwindcss(),
+  ],
+
+})
